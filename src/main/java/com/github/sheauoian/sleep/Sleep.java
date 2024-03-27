76cace1041c5989a1a558dbd76a161682af287b9
Sheauoian <48637290+sheauoian@users.noreply.github.com>
2024-03-27 12:51:44 +0900

    Oh no

--- src/main/java/com/github/sheauoian/sleep/Sleep.java
+++ src/main/java/com/github/sheauoian/sleep/Sleep.java
@@ -3,15 +3,18 @@ package com.github.sheauoian.sleep;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Sleep extends JavaPlugin {
+    public static Sleep instance;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        instance = this;
+        getLogger().info("she slept ;)");
     }
-
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+    public Sleep getInstance() {
+        return instance;
     }
 }
---
+++ src/main/java/com/github/sheauoian/sleep/command/SleepCMD.java
@@ -0,0 +1,32 @@
+package com.github.sheauoian.sleep.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class SleepCMD implements CommandExecutor, TabCompleter {
+    @Override
+    public boolean onCommand(
+            @NotNull CommandSender commandSender,
+            @NotNull Command command,
+            @NotNull String s,
+            @NotNull String[] strings)
+    {
+        return false;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(
+            @NotNull CommandSender commandSender,
+            @NotNull Command command,
+            @NotNull String s,
+            @NotNull String[] strings)
+    {
+        return null;
+    }
+}
